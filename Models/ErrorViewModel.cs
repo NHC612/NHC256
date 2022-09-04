eca7707cc14c5c0b5f2b9c88f114502d13064ec9
NHC612 <hcam418@gmail.com>
2022-09-04 16:27:46 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nguyen_Hoang_Cam__BTH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
