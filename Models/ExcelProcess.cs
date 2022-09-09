f3bc82f390fd60cd922f6b33c8e4d12a23e2ea44
NHC612 <hcam418@gmail.com>
2022-09-09 19:42:27 +0700

    Nguyenhoangcam_0709

---
+++ Models/ExcelProcess.cs
@@ -0,0 +1,12 @@
+namespace DemoMVC.Models;
+
+public class ExcelProcess
+{
+    //khai báo các phương thức thược tính 
+    //khai báo 1 phuong tinh tong 2 so 
+    
+    public int ExcelProcessID { get; set; }
+    public int ExcelProcessNameAge { get; set; }
+    public char ExcelProcessName { get; set; }
+    
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+namespace NguyenHoangCamBTH.Models
+{
+    public class student
+    {
+        //các phuong thuc hoac thuoc tinh
+        public string  StudentID { get; set; }
+        public int StudentName { get; set; }
+        public char StudentAge { get; set; }
+    }
+}
