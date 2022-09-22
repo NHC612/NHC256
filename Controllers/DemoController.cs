173fb73a4b22c78ae86a0efe3e4db266cf57c4a9
NHC612 <hcam418@gmail.com>
2022-09-22 20:36:10 +0700

    NguyenHoangCam-2209

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -25,5 +25,17 @@ namespace MvcMovie.Controllers
             ViewBag.abc = mess;
             return View();
         }
+        [HttpPost]
+
+        public IActionResult Create(string heSoA, string heSoB, string heSoC)
+        {
+            // ep kieu du lieu cua tham so
+            double a = Convert.ToDouble(heSoA);
+            double b = Convert.ToDouble(heSoB);
+            double c = Convert.ToDouble(heSoC);
+            string mess = gpt.GiaiPhuongTrinhBacHai(a, b, c);
+            ViewBag.mess = mess;
+            return View();
+        }
     }
 }
